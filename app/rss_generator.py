6e0c15a6e7b1190788ca34ca90219e8e3678c7f3
Golumpa <admin@golumpa.moe>
2024-12-23 19:51:01 +0000

    update rss gen

--- app/rss_generator.py
+++ app/rss_generator.py
@@ -29,15 +29,14 @@ def format_snapshot_data(snapshot):
         return f"Error: Unable to parse snapshot data for Anilist ID {snapshot.anilist_id}"
 
     formatted_output = f"""
-<img src="{snapshot.cover_image_url}" alt="{snapshot.anime_title}" style="float: left; margin-right: 10px;"/>
-<h2>There has been an update for the SeaDex entry for {snapshot.anime_title}.</h2>
+<a href="https://releases.moe/{snapshot.anilist_id}/"><img src="{snapshot.cover_image_url}" alt="{snapshot.anime_title}" style="float: left; margin-right: 10px;"/>
+<h2>There has been an update for the SeaDex entry for {snapshot.anime_title}.</h2></a>
 <p>Below is the latest version of the SeaDex entry.</p>
 <br>
 <ul>
     <li><strong>Updated:</strong> {data.get('updated', 'N/A')}</li>
     <li><strong>Comparison:</strong> {format_comparison_links(data.get('comparison'))}</li>
     <li><strong>Complete?:</strong> <span style="color: {'green' if not data.get('incomplete', True) else 'red'};">{not data.get('incomplete', True)}</span></li>
-    <br>
     <li><strong>Notes:</strong> {data.get('notes', 'N/A')}</li>
 </ul>
 <div style="clear: both">
@@ -52,8 +51,8 @@ def format_snapshot_data(snapshot):
 <h4><u>{torrent.get('releaseGroup', 'Unknown Group')}</u></h4>
 <ul>
     <li><strong>Size:</strong> {total_size_gib:.2f} GiB</li>
-    <li><strong>Is Best?:</strong> <span style="color: {'green' if torrent.get('isBest') else 'red'};">{torrent.get('isBest', 'No')}</span></li>
-    <li><strong>Dual Audio?:</strong> <span style="color: {'green' if torrent.get('dualAudio') else 'red'};">{torrent.get('dualAudio', 'No')}</span></li>
+    <li><strong>Is Best?:</strong> <span style="color: {'green' if torrent.get('isBest') else 'red'};">{torrent.get('isBest', 'False')}</span></li>
+    <li><strong>Dual Audio?:</strong> <span style="color: {'green' if torrent.get('dualAudio') else 'red'};">{torrent.get('dualAudio', 'False')}</span></li>
     <li><strong>Tracker:</strong> {torrent.get('tracker', 'N/A')}</li>
     <li><strong>Torrent Url:</strong> <a href="{torrent.get('url', '#')}">Link</a></li>
 </ul>
@@ -75,7 +74,7 @@ def generate_rss(snapshots, anilist_id):
         fe = fg.add_entry()
         formatted_date = snapshot.timestamp.strftime("%d/%m/%Y")
         fe.title(f"Update on {formatted_date}")
-        fe.link(href=f"https://anilist.co/anime/{anilist_id}")
+        fe.link(href=f"https://releases.moe/{anilist_id}/")
 
         formatted_data = format_snapshot_data(snapshot)
         fe.description(formatted_data)
